300a435f774b068bcac5a68f692720ee9d99f1f8
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-03-11 08:44:28 +0100

    adjust bundle structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-.setOutputPath('src/Resources/public/assets')
-.addEntry('contao-be-explanation-bundle', './src/Resources/assets/js/contao-be_explanation-bundle.js')
+.setOutputPath('public/assets')
+.addEntry('contao-be-explanation-bundle', './assets/js/contao-be_explanation-bundle.js')
 .setPublicPath('/bundles/heimrichhannotcontaobeexplanation/assets')
 .setManifestKeyPrefix('bundles/heimrichhannotcontaobeexplanation/assets')
 .disableSingleRuntimeChunk()
